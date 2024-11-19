99f795088e3d762c3464968b9d6b3da872d10116
Amrit-2708 <rrajamrit.21@gmail.com>
2024-11-19 16:54:22 +0530

    Footer justified for mobile devices

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,29 +7,29 @@ const Footer = () => {
           <img
             src="/logo.png"
             alt="logo.png"
-            className=" w-12 h-12 mix-blend-multiply"
+            className="w-[4vw] mix-blend-multiply"
           />
           <h1
-            className="text-3xl font-bold my-2"
+            className="text-3xl font-bold"
             style={{ fontFamily: "Papyrus" }}
           >
             Contact Us
           </h1>
         </div>
-        <div className="md:mt-0 mt-5 ml-8">
-          <div className="flex items-center mt-2">
-            <img className="w-[3vw]" src="/phone.png" alt="" />
+        <div className="md:mt-0 mt-5">
+          <div className="flex flex-row justify-center items-center mt-2">
+            <img className="w-[4vw]" src="/phone.png" alt="" />
             <p className="ml-1">+91 123456789</p>
           </div>
         </div>
         <div className="md:mt-0 mt-5">
-          <div className="flex items-center mt-2 mr-5">
-            <img className="w-[3vw]" src="/email.png" alt="" />
+          <div className="flex flex-row justify-center items-center mt-2">
+            <img className="w-[4vw]" src="/email.png" alt="" />
             <p className="ml-1">Flavorsome@gmail.com</p>
           </div>
         </div>
       </footer>
-      <div className="bg-white py-4 flex flex-col md:flex-row justify-center">
+      <div className="bg-white py-4 flex flex-row md:flex-row justify-center">
         <div className="mb-2">
           &copy; 2024 Your Food Order Website. All rights reserved.
         </div>
